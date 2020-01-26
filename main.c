15db9d2ec55e93dcc371c151ee83251ec1be7e2d
shirogin <42269089+shirogin@users.noreply.github.com>
2020-01-26 20:37:15 +0100

    ezrg

--- main.c
+++ main.c
@@ -1,13 +1,54 @@
 char *yytext;
 #include "./MiniAcad/Lexical/Lexical.h"
 List * Expression=NULL;
+
+
+
+List * Postfix(List *expression){
+    List *postfix =NULL;
+    Noed *checker=expression->Head;
+    List *operators=NULL;
+    while (checker!=NULL)
+    {
+        if(strlen(checker->val)>1){
+            Pushf(&postfix,"dd");
+        }else
+        {
+            switch (checker->val[0])
+            {
+                case '+':
+                case '-':
+                case '/':
+                case '*':
+                    // oper
+                    if(checker->val[0])
+                    break;
+                case '(':
+                    //open
+                    break;
+                case ')':
+                    //close
+                    break;
+                case 'm':
+
+                    break;
+                default:
+                    //error
+                    break;
+            }
+        }
+        
+        checker=checker->next;
+    }
+    
+}
 int main(){
     Pushf(&Expression,"A");
     Pushf(&Expression,"=");
     Pushf(&Expression,"A");
     Pushf(&Expression,"+");
     Pushf(&Expression,"C");
-    printf("work %s\n",Expression->Head->val);
     PrintL(Expression);
+    printf("work \n");
     return 0;
 }
